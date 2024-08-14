6c12dc73ceedd0d41cd9009e7c94abcd6df9fce6
Faycal <bessayahf@gmail.com>
2024-08-14 19:59:18 +0200

    Initial Commit

---
+++ Inventory Management/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Inventory Management
+//
+//  Created by Faycal Bessayah on 14/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
---
+++ Inventory Management/Inventory_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Inventory_ManagementApp.swift
+//  Inventory Management
+//
+//  Created by Faycal Bessayah on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Inventory_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
